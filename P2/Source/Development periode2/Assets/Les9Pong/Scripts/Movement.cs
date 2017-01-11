1ea12e501b55982e4e96d8dee551b20452470267
Soraya Timmers <sorayatimmers@hotmail.com>
2017-01-11 13:48:25 +0100

    ProjectOnderdelen

---
+++ P2/Source/Development periode2/Assets/Les9Pong/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+
+    public Vector3 axis;
+
+    public float ver;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ver = Input.GetAxis("Vertical");
+
+        axis.z = ver;
+	    
+	}
+}
