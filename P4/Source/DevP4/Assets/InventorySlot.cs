60709adb7e50ffeee40150788f2e3c6619ff312d
Soraya Timmers <sorayatimmers@hotmail.com>
2017-05-03 16:17:55 +0200

    Documenten P4

---
+++ P4/Source/DevP4/Assets/InventorySlot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySlot : MonoBehaviour
+{
+    public List<GameObject> itemList = new List<GameObject>();
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ P4/Source/DevP4/Assets/Item.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    //het 3d object(item)
+    public GameObject swordObject;
+    //de sprite(item)
+    public Sprite swordSprite;
+
+    //het gewicht van het wapen
+    public int weightStat;
+    //de max snelheid die de speler met de item heeft
+    public int speedStat;
+    //de kracht van de item
+    public int strenghtStat;
+    //de verdedig van de item
+    public int defenseStat;
+    //hoeveel de item heelt
+    public int healthStat;
+    //de afstand die het item kan afleggen
+    public int rangeStat;
+    //de hoeveelheid magie dat het item bevat
+    public int magicStat;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
