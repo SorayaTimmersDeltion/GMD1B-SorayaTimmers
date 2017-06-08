d13e30f3a82ec964afb5a2fe288e722d99f862cb
Soraya Timmers <sorayatimmers@hotmail.com>
2017-06-08 10:41:08 +0200

    P4 3D+ dev + docs

---
+++ P4/Source/DevP4/Assets/DataSave/Scripts/DataHolder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class DataHolder
+{
+    public int test;
+    public float f;
+    public bool b;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ P4/Source/DevP4/Assets/DataSave/Scripts/DataManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    public DataHolder dataHolder;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //dataHolder = new DataHolder();
+
+        dataHolder = Load();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Save()
+    {
+        var serializer = new XmlSerializer(typeof(DataHolder));
+
+        using (var stream = new FileStream(Application.dataPath + "/Testsave.xml", FileMode.Create))
+        {
+            serializer.Serialize(stream, dataHolder);
+        }
+    }
+
+    public DataHolder Load()
+    {
+        var serializer = new XmlSerializer(typeof(DataHolder));
+
+        using (var stream = new FileStream(Application.dataPath + "/Testsave.xml", FileMode.Open))
+        {
+            return serializer.Deserialize(stream) as DataHolder;
+        }
+    }
+}
