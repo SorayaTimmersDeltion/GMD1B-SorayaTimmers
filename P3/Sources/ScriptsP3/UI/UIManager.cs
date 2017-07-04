9b3b5d0486906ee9da852a860b7452642a2537d5
Soraya Timmers <sorayatimmers@hotmail.com>
2017-07-04 20:02:40 +0200

    P3 scripts

--- P3/Sources/ScriptsP3/UI/UIManager.cs
+++ P3/Sources/ScriptsP3/UI/UIManager.cs
@@ -5,15 +5,11 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    public Text dialogText;
+    public GameObject textButton;
+    public Text dialogueText;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	void Start ()
+    {
+        textButton.SetActive(false); 
 	}
 }
