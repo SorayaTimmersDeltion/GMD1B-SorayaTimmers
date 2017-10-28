7583dc4f740470f4815c96c5c0311cec35bf2de7
Soraya Timmers <sorayatimmers@hotmail.com>
2017-10-28 12:24:07 +0200

    J2P1

---
+++ J2/Lessen/Creatief profileren/Snake/Assets/Scripts/Snake_movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake_movement : MonoBehaviour
+{
+    public Vector3 axis;
+    public float horMovement;
+    public float vertMovement;
+    public float speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        SnakeMovement();
+    }
+
+    public void SnakeMovement()
+    {
+        horMovement = Input.GetAxis("Horizontal");
+        axis.x = horMovement;
+
+        vertMovement = Input.GetAxis("Vertical");
+        axis.z = vertMovement;
+
+        transform.forward = axis;
+
+        gameObject.transform.Translate(transform.forward * Time.deltaTime * speed);
+
+    }
+}
