af7f6e943be5f45c117c621b70c4fab2b59bf774
Soraya Timmers <sorayatimmers@hotmail.com>
2018-02-09 10:37:22 +0100

    J2

---
+++ J2/Projecten/P2/Prototype/Prototype1/CharacterAnimation/Assets/Prototype1/CharacterMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public Animator characterAnim;
+    public bool isRunningPressed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        characterAnim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Running();
+	}
+
+    public void Running()
+    {
+        isRunningPressed = Input.GetButton("Jump");
+        characterAnim.SetBool("IsRunning", isRunningPressed);
+    }
+
+}
